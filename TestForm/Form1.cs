06380331c5b67e8b9f8c407db863ea5648885e93
deusmemes <maxin29999@gmail.com>
2019-09-17 15:24:22 +0400

    add select component

---
+++ TestForm/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            controlListBoxSelected1.LoadEnumeration(new List<string>{"Москва", "Ульяновск", "Киев"});
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var city = controlListBoxSelected1.SelectedText;
+
+            if (!city.Equals(""))
+            {
+                MessageBox.Show(
+                    $"Выбран город {city}");
+            }
+            else
+            {
+                MessageBox.Show("Город не выбран");
+            }
+        }
+    }
+}
