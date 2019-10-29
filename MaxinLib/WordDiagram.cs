29261f6f4e8373cea50ddec8979ef4dc7921befa
deusmemes <maxin29999@gmail.com>
2019-10-29 13:35:14 +0300

    bar chart

--- MaxinLib/WordDiagram.cs
+++ MaxinLib/WordDiagram.cs
@@ -32,40 +32,9 @@ namespace MaxinLib
             // создаём документ
             DocX document = DocX.Create(pathDocument);
 
-            document.InsertChart(CreateLineChart());
             document.InsertChart(CreateBarChart());
-            document.InsertChart(CreatePieChart());
-            document.InsertChart(Create3DBarChart());
 
             document.Save();
-        }
-
-        private static Chart CreateLineChart()
-        {
-            // создаём линейную диаграмму
-            LineChart lineChart = new LineChart();
-            // добавляем легенду вниз диаграммы
-            lineChart.AddLegend(ChartLegendPosition.Bottom, false);
-
-            // создаём набор данных и добавляем на диаграмму
-            lineChart.AddSeries(TestData.GetSeriesFirst());
-            // добавляем ещё один набор
-            lineChart.AddSeries(TestData.GetSeriesSecond());
-
-            return lineChart;
-        }
-
-        private static Chart CreatePieChart()
-        {
-            // создаём круговую диаграмму
-            PieChart pieChart = new PieChart();
-            // добавляем легенду слева от диаграммы
-            pieChart.AddLegend(ChartLegendPosition.Left, false);
-
-            // создаём набор данных и добавляем на диаграмму
-            pieChart.AddSeries(TestData.GetSeriesFirst());
-
-            return pieChart;
         }
 
         private static Chart CreateBarChart()
@@ -77,25 +46,6 @@ namespace MaxinLib
 
             // создаём набор данных и добавляем в диаграмму
             barChart.AddSeries(TestData.GetSeriesFirst());
-            // создаём набор данных и добавляем в диаграмму
-            barChart.AddSeries(TestData.GetSeriesSecond());
-
-            return barChart;
-        }
-
-        private static Chart Create3DBarChart()
-        {
-            // создаём столбцовую диаграмму
-            BarChart barChart = new BarChart();
-            // отображаем легенду снизу диаграммы
-            barChart.AddLegend(ChartLegendPosition.Bottom, false);
-            // добавление 3D эффекта
-            barChart.View3D = true;
-
-            // создаём набор данных и добавляем в диаграмму
-            barChart.AddSeries(TestData.GetSeriesFirst());
-            // создаём набор данных и добавляем в диаграмму
-            barChart.AddSeries(TestData.GetSeriesSecond());
 
             return barChart;
         }
